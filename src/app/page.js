3daef8f733ab5327ce9ffe454cbdc7df093e98b9
Kevin He <ipman4567@gmail.com>
2024-10-13 19:32:20 -0400

    added blueberry animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,69 +1,87 @@
 "use client";
+
+import { useEffect } from "react";
+
 const handleSubmit = () => {
-  document
-    .getElementById("waitlist-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+  event.preventDefault();
 
-      const email = document.getElementById("email").value;
-      const formData = new FormData();
-      formData.append("email", email);
+  const email = document.getElementById("email").value;
+  const formData = new FormData();
+  formData.append("email", email);
 
-      fetch(
-        "https://script.google.com/macros/s/AKfycbz-K_O8FdJX2yYK-FAUzIdNB3l0Bs78A7XZGqfo-PTSZZ_CPglRal_Z3xuP_YXzDeLwXQ/exec",
-        {
-          method: "POST",
-          body: formData,
-        }
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          alert("You've successfully joined the waitlist!");
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          alert("Something went wrong. Please try again.");
-        });
+  fetch(
+    "https://script.google.com/macros/s/AKfycbz-K_O8FdJX2yYK-FAUzIdNB3l0Bs78A7XZGqfo-PTSZZ_CPglRal_Z3xuP_YXzDeLwXQ/exec",
+    {
+      method: "POST",
+      body: formData,
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert("You've successfully joined the waitlist!");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Something went wrong. Please try again.");
     });
 };
 
 export default function Home() {
+  useEffect(() => {
+    const form = document.getElementById("waitlist-form");
+    if (form) {
+      form.addEventListener("submit", handleSubmit);
+    }
+
+    return () => {
+      if (form) {
+        form.removeEventListener("submit", handleSubmit);
+      }
+    };
+  }, []);
+
   return (
-    <div class="bg-black min-h-screen flex flex-col items-center justify-center text-center">
-      <div class="absolute top-4 left-4 z-0">
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center text-center">
+      <div className="absolute top-4 left-4 z-0">
         <img
           src="./PickyEats.png"
           alt="PickyEats Logo"
-          class="w-32 h-32 object-contain"
+          className="w-32 h-32 object-contain"
         />
       </div>
 
-      <div class="text-white max-w-md md:max-w-lg absolute">
-        <h1 class="text-5xl md:text-7xl font-bold mb-6 font-patuaOne z-10">
+      <div className="text-white max-w-md md:max-w-lg absolute">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 font-patuaOne z-10">
           Ready to Eat?
         </h1>
-        <h2 class="text-2xl mb-6 font-patuaOne">PickyEats is almost here...</h2>
-
-        <form id="waitlist-form" class="flex flex-col items-center space-y-4">
+        <div className="flex flex-row justify-center">
+          <h2 className="text-2xl mb-6 font-patuaOne">
+            PickyEats is almost here...
+          </h2>
+          <p className="mt-2 animate-bounce">🫐</p>
+        </div>
+        <form
+          id="waitlist-form"
+          className="flex flex-col items-center space-y-4"
+        >
           <input
             type="email"
             id="email"
             name="email"
-            class="w-full p-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-pink-500"
+            className="w-full p-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-pink-500"
             placeholder="Enter your email"
             required
           />
           <button
             type="submit"
-            class="w-full bg-pink-400 font-patuaOne hover:bg-white transition-all hover:text-black text-white py-3 rounded-lg font-bold duration-200"
-            onClick={handleSubmit}
+            className="w-full bg-pink-400 font-patuaOne hover:bg-white transition-all hover:text-black text-white py-3 rounded-lg font-bold duration-200"
           >
             Join the Waitlist
           </button>
         </form>
 
-        <p class="mt-4 text-gray-400 font-patuaOne">
+        <p className="mt-4 text-gray-400 font-patuaOne">
           Be the first to know when PickyEats launches!
         </p>
       </div>
