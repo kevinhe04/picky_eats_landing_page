305081ac0b7568a0ad11d2b9e13e4d0dc847bb61
Kevin He <ipman4567@gmail.com>
2024-10-30 14:40:06 -0400

    removed the waitlist and added app store and google play logos

--- src/app/page.js
+++ src/app/page.js
@@ -1,87 +1,42 @@
 "use client";
 
-import { useEffect } from "react";
-
-const handleSubmit = () => {
-  event.preventDefault();
-
-  const email = document.getElementById("email").value;
-  const formData = new FormData();
-  formData.append("email", email);
-
-  fetch(process.env.NEXT_PUBLIC_GOOGLE_SCRIPT_URL, {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      alert("You've successfully joined the waitlist!");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert("Something went wrong. Please try again.");
-    });
-};
+import StoreBadge from "react-store-badge";
 
 export default function Home() {
-  useEffect(() => {
-    const form = document.getElementById("waitlist-form");
-    if (form) {
-      form.addEventListener("submit", handleSubmit);
-    }
-
-    return () => {
-      if (form) {
-        form.removeEventListener("submit", handleSubmit);
-      }
-    };
-  }, []);
-
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center text-center">
-      <div className="absolute top-4 left-4 z-0">
+      <div className="absolute top-0 left-50% z-0">
         <img
           src="./PickyEats.png"
           alt="PickyEats Logo"
-          className="w-32 h-32 object-contain"
+          className="w-64 h-64 object-contain "
         />
       </div>
 
       <div className="text-white max-w-md md:max-w-lg absolute">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 font-patuaOne z-10">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 font-patuaOne z-10 mt-10">
           Ready to Eat?
         </h1>
         <div className="flex flex-row justify-center">
           <h2 className="text-2xl mb-6 font-patuaOne">
-            PickyEats Mobile is almost here...
+            PickyEats is almost here...
           </h2>
           <p className="mt-2 animate-bounce">🫐</p>
         </div>
-        <form
-          id="waitlist-form"
-          className="flex flex-col items-center space-y-4"
-        >
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full p-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-pink-500"
-            placeholder="Enter your email"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full bg-pink-400 font-patuaOne hover:bg-white transition-all hover:text-black text-white py-3 rounded-lg font-bold duration-200"
-          >
-            Join the Waitlist for PickyEats Mobile !
-          </button>
-        </form>
 
-        <p className="mt-4 text-gray-400 font-patuaOne">
-          Be the first to know when PickyEats launches on IOS and Google Play!
-        </p>
-        <p className="mt-4 text-gray-400 font-patuaOne">
+        <div className="flex flex-row gap-5 justify-center">
+          <StoreBadge
+            name="PickyEats"
+            googlePlayUrl="https://play.google.com/"
+            target="_blank"
+          />
+          <StoreBadge
+            name="PickyEats"
+            appStoreUrl="https://apps.apple.com/"
+            target="_blank"
+          />
+        </div>
+        <p className="mt-10 text-gray-400 font-patuaOne">
           In the meantime, go checkout the web version of PickyEats!
           <br></br>
           👉
